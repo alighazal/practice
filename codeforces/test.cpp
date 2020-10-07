e94a9fe9325b28a79e2796a93e4ae3f8244f6c26
alighazal <ali.mdghazal@gmail.com>
2020-10-07 02:55:39 +0200

    change user-test

---
+++ codeforces/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int test() {
+    
+    
+    return 5;
+    
+    
+}
+
+
+int main () {
+
+    cout << test() << endl;
+
+    return 0;
+}
